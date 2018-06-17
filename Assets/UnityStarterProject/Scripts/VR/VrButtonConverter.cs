06bc32fc0a8b59765c6178e5d76fa5f32d81a31a
Allen Brower <allenmbrower@gmail.com>
2018-06-16 23:40:19 -0400

    Added in a VR version of the main menu you can manipulate with the VR controllers Other tweaks and changes for VR

---
+++ Assets/UnityStarterProject/Scripts/VR/VrButtonConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Valve.VR.InteractionSystem;
+
+namespace UnityStarterProject.VR.UI
+{
+    public class VrButtonConverter : Singleton<VrButtonConverter>
+    {
+        private void Start()
+        {
+            GameObject[] rootGameObjects = SceneManager.GetActiveScene().GetRootGameObjects();
+
+            foreach (GameObject go in rootGameObjects)
+            {
+                foreach(Button button in go.GetComponentsInChildren<Button>(true))
+                {
+                    RectTransform buttonRect = button.transform as RectTransform;
+                    BoxCollider box = button.gameObject.AddComponent<BoxCollider>();
+                    box.size = new Vector3(buttonRect.rect.width, buttonRect.rect.height, 5f);
+                    UIElement uiElement = button.gameObject.AddComponent<UIElement>();
+                }
+            }
+        }
+    }
+}
